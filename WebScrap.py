5b98a55388d9515a046f1a8697a9706d9c78b96a
Brocowlee <arthur.gonay@gmail.com>
2022-05-10 16:50:13 +0200

    avancement webScrap

--- WebScrap.py
+++ WebScrap.py
@@ -3,61 +3,73 @@ import selenium
 import requests
 
 url="https://www.justwatch.com/fr/films"
-urlfilm="https://www.justwatch.com/fr/film/spider-man-no-way-home"
+urlfilm="https://www.justwatch.com/fr/film/les-animaux-fantastiques"
 result=requests.get(urlfilm)
 soup = BeautifulSoup(result.text, 'html.parser')
 
 
-def findLink(text):
-    cross=False
-    res=""
-    lastcar=""
-    for car in text:
-        if car=='"' and cross==True:
-            return res
-        elif car=='/' and lastcar=='"' and cross==False:
-            cross=True
-        elif cross==True:
-            res+=car
-        lastcar=car
-
-# for e in soup.find_all("a",class_="title-list-grid__item--link"):
-#     print(findLink(str(e)))
-
-def getReal(link):
-    result=requests.get(link)
-    soup = BeautifulSoup(result.text, 'html.parser')
-    res=""
-    cross=False
-    line=soup.find_all("a",class_="title-credit-name")[0]
-    for car in str(line):
-        if car=="<" and cross==True:
-            res=res[1:-1]
-            return res
-        elif cross==True:
-            res+=car
-        elif car==">" and cross==False:
-            cross=True
-
-
-def getCast(link):
-    result=requests.get(link)
-    soup = BeautifulSoup(result.text, 'html.parser')
+def findLinks():
     res=[]
-    for line in soup.find_all("a",class_="title-credit-name"):
-        cross=False
-        name=""
-        for car in str(line):
-            if car=="<" and cross==True:
-                name=name[1:-1]
-                res.append(name)
-                break
-            elif cross==True:
-                name+=car
-            elif car==">" and cross==False:
-                cross=True
+    for link in soup.find_all("a",{"class":"title-list-grid__item--link"}):
+        res.append("https://www.justwatch.comlink"+link["href"])
+    return res
+
+
+
+
+def getReal(soup):
+    return soup.find_all("a",class_="title-credit-name")[0].text
+
+
+def getCast(soup):
+    res=[]
+    for cast in soup.find_all("a",class_="title-credit-name"):
+        res.append(cast.text)
     res.pop(0)
     res.pop(0)
     return res
 
-print(getCast(urlfilm))
+def getImage(soup):
+    res=""
+    line = soup.find_all("source",{"type":"image/jpeg"})[3]["data-srcset"]
+    for car in line:
+        if car ==",":
+            return res
+        else:
+            res+=car
+
+def getGenre(soup):
+    res=[]
+    line=soup.find_all("div",{"class":"detail-infos__value"})[1].text
+    lstGenre=list(line.split(" , "))
+    lstGenre[len(lstGenre)-1]=lstGenre[len(lstGenre)-1][:-1]
+    return lstGenre
+
+def getResume(soup):
+    line=soup.find("p",{"class":"text-wrap-pre-line mt-0"}).text
+    return line
+
+def getTitre(soup):
+    return soup.find("h1").text #contient des espaces au début et à la fin
+
+def getAnnee(soup):
+    return soup.find("span",{"class":"text-muted"}).text[2:-2]
+
+def getDuree(soup):
+    return soup.find_all("div",{"class":"detail-infos__value"})[2].text
+
+
+def initTxt(link):
+    result=requests.get(link)
+    soup = BeautifulSoup(result.text, 'html.parser')
+    with open('films.txt', 'w',encoding="") as f:
+        f.write("INSERT INTO Film(titre,resume,annee_sortie,duree) VALUES ('"+getTitre(soup)+"','"+getResume(soup)+"','"+getAnnee(soup)+"','"+getDuree(soup))
+
+def initTxtGlobal():
+    with open('films.txt', 'w') as f:
+        for link in findLinks():
+            result=requests.get(link)
+            soup = BeautifulSoup(result.text, 'html.parser')
+            f.write(getTitre(link))
+
+#print(initTxt(urlfilm))
