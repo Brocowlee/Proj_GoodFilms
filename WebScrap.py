eeac62e36310f3c3596f53f59ac80ca2a7eec842
Brocowlee <arthur.gonay@gmail.com>
2022-05-15 15:40:22 +0200

    webScrapping avec autoIncrement final

--- WebScrap.py
+++ WebScrap.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
 import selenium
 import requests
+from time import sleep
 
 
 id_f=0
+lstFilms=[]
 lstPers=[]
 lstGenre=[]
 
@@ -62,6 +64,9 @@ def getGenre(soup):
 
 def getResume(soup):
     line=soup.find("p",{"class":"text-wrap-pre-line mt-0"}).text
+    for i in range(len(line)):
+        if line[i]=='"':
+            return None
     return line
 
 def getTitre(soup):
@@ -95,38 +100,38 @@ def initTxt(link):
     soup = BeautifulSoup(result.text, 'html.parser')
     with open('films.txt', 'a',encoding="utf-8") as f:
         try:
-            f.writelines('\nINSERT INTO Film(id_f,titre,resume,annee_sortie,duree,image) VALUES ('+str(id_f)+',"'+getTitre(soup)+'","'+getResume(soup)+'","'+getAnnee(soup)+'","'+getDuree(soup)+'","'+getImage(soup)+'");\n')
+            f.writelines('\nINSERT INTO Film(titre,resume,annee_sortie,duree,image) VALUES ("'+getTitre(soup)+'","'+getResume(soup)+'","'+getAnnee(soup)+'","'+getDuree(soup)+'","'+getImage(soup)+'");\n')
+            lstFilms.append(link)
             idP=isAldreadyAddPers(soup, getReal(soup))
             if idP==None:
                 lstPers.append(getReal(soup))
                 prenom=getReal(soup).split(" ")[1]
                 nom=getReal(soup).split(" ")[2]
-                f.writelines('INSERT INTO Personne(id_p,nom,prenom) VALUES ('+str(len(lstPers)-1)+',"'+nom+'","'+prenom+'");\n')
-                f.writelines("INSERT INTO realise(id_f, id_p) VALUES ("+str(id_f)+","+str(len(lstPers)-1)+");\n")
+                f.writelines('INSERT IGNORE INTO Personne(nom,prenom) VALUES ("'+nom+'","'+prenom+'");\n')
+                f.writelines("INSERT INTO realise(id_f, id_p) VALUES ("+str(len(lstFilms))+","+str(len(lstPers))+");\n")
             else:
-                f.writelines("INSERT INTO realise(id_f, id_p) VALUES ("+str(id_f)+","+str(idP)+");\n")
+                f.writelines("INSERT INTO realise(id_f, id_p) VALUES ("+str(len(lstFilms))+","+str(idP)+");\n")
             for cast in getCast(soup):
                 idP=isAldreadyAddPers(soup, cast)
                 if idP== None:
                     lstPers.append(cast)
                     prenom=cast.split(" ")[1]
                     nom=cast.split(" ")[2]
-                    f.writelines('INSERT INTO Personne(id_p,nom,prenom) VALUES ('+str(len(lstPers)-1)+',"'+nom+'","'+prenom+'");\n')
-                    f.writelines("INSERT INTO joue(id_f, id_p) VALUES ("+str(id_f)+","+str(len(lstPers)-1)+");\n")
+                    f.writelines('INSERT IGNORE INTO Personne(nom,prenom) VALUES ("'+nom+'","'+prenom+'");\n')
+                    f.writelines("INSERT INTO joue(id_f, id_p) VALUES ("+str(len(lstFilms))+","+str(len(lstPers))+");\n")
                 else:
-                    f.writelines("INSERT INTO joue(id_f, id_p) VALUES ("+str(id_f)+","+str(idP)+");\n")
+                    f.writelines("INSERT INTO joue(id_f, id_p) VALUES ("+str(len(lstFilms))+","+str(idP)+");\n")
             for genre in getGenre(soup):
                 idG=isAldreadyAddGenre(soup, genre)
                 if idG!=None:
-                    f.writelines("INSERT INTO genres2films(id_g,id_f) VALUES ("+str(idG)+","+str(id_f)+");\n")
+                    f.writelines("INSERT INTO genres2films(id_g,id_f) VALUES ("+str(idG)+","+str(len(lstFilms))+");\n")
                 else:
                     lstGenre.append(genre)
-                    f.writelines("INSERT INTO Genre(id_g,genre) VALUES ("+str(len(lstGenre)-1)+",'"+genre+"');\n")
-                    f.writelines("INSERT INTO genre2films(id_g,id_f) VALUES ("+str(len(lstGenre)-1)+","+str(id_f)+");\n")
+                    f.writelines("INSERT INTO Genre(id_g,genre) VALUES ("+str(len(lstGenre))+",'"+genre+"');\n")
+                    f.writelines("INSERT INTO genres2films(id_g,id_f) VALUES ("+str(len(lstGenre))+","+str(len(lstFilms))+");\n")
 
         except:
-            pass
-        id_f+=1
+            print("error")
         f.close()
     
 
@@ -146,8 +151,24 @@ def initTxtGlobal(soup):
 #print(initTxt(urlfilm))
 #initTxtGlobal(soup)
 
-urls=["https://www.justwatch.com/fr/films?release_year_from=2022","https://www.justwatch.com/fr/films?release_year_from=2021&release_year_until=2021","https://www.justwatch.com/fr/films?release_year_from=2020&release_year_until=2020","https://www.justwatch.com/fr/films?release_year_from=2019&release_year_until=2019","https://www.justwatch.com/fr/films?release_year_from=2018&release_year_until=2018","https://www.justwatch.com/fr/films?release_year_from=2017&release_year_until=2017","https://www.justwatch.com/fr/films?release_year_from=2016&release_year_until=2016"]
+urls=["https://www.justwatch.com/fr/films?release_year_from=2022","https://www.justwatch.com/fr/films?release_year_from=2021&release_year_until=2021","https://www.justwatch.com/fr/films?release_year_from=2020&release_year_until=2020","https://www.justwatch.com/fr/films?release_year_from=2019&release_year_until=2019","https://www.justwatch.com/fr/films?release_year_from=2018&release_year_until=2018","https://www.justwatch.com/fr/films?release_year_from=2017&release_year_until=2017","https://www.justwatch.com/fr/films?release_year_from=2016&release_year_until=2016","https://www.justwatch.com/fr/films?release_year_from=2015&release_year_until=2015"]
+url="https://www.justwatch.com/fr/film/fantastic-beasts-3"
+with open('films.txt', 'a',encoding="utf-8") as f:
+     f.writelines('TRUNCATE TABLE Film;\n')
+     f.writelines('TRUNCATE TABLE Genre;\n')
+     f.writelines('TRUNCATE TABLE genres2films;\n')
+     f.writelines('TRUNCATE TABLE joue;\n')
+     f.writelines('TRUNCATE TABLE Personne;\n')
+     f.writelines('TRUNCATE TABLE realise;\n')
+     f.close()
+
 for url in urls:
     result=requests.get(url)
     soup = BeautifulSoup(result.text, 'html.parser')
     initTxtGlobal(soup)
+
+
+# result=requests.get(url)
+# soup = BeautifulSoup(result.text, 'html.parser')
+# #print(getResume(soup))
+# print(getCast(soup))#base >
