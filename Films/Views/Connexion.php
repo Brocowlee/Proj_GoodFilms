0d6fbee30dc3f67ac7fb9b47434d2f30a242a10c
Brocowlee <arthur.gonay@gmail.com>
2022-05-27 01:14:10 +0200

    update css connexion

--- Films/Views/Connexion.php
+++ Films/Views/Connexion.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-
+<link href="css\Connexion.css" rel="stylesheet">
 <head>
     <meta charset="UTF-8">
 </head>
@@ -16,13 +16,14 @@
         <form id="informations" method="POST">
             login : <input type="text" name="login" required></br>
             mot de passe : <input type="password" name="password" required></br>
-            <button type="submit" name="action" value="connexion">Connexion</buttom>
+            <button id="connexionBtn" type="submit" name="action" value="connexion">Connexion</buttom>
         </form>
         <form id="send_to_inscription" method="POST">
-            <button type="submit" name="action" value="send_to_inscription">Inscription</buttom>
+            <button id="inscriptionBtn" type="submit" name="action" value="send_to_inscription">Inscription</buttom>
         </form>
       
 
     </div>
-
+    <?php include_once ("footer.php"); ?>
 </body>
+</html>
