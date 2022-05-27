42c4498c4ef7403d3b312b18eb3275e941dff5c3
Brocowlee <arthur.gonay@gmail.com>
2022-05-27 14:43:15 +0200

    page connexion final normalement

--- Films/Views/Connexion.php
+++ Films/Views/Connexion.php
@@ -14,8 +14,8 @@
         <h2>Connexion</h2>
         
         <form id="informations" method="POST">
-            login : <input id="barreLogin" type="text" name="login" required></br>
-            mot de passe : <input id="barreMdp" type="password" name="password" required></br>
+            <div id="login">Login :</div> <input id="barreLogin" type="text" name="login" required></br>
+            <div id="mdp">Mot de passe :</div> <input id="barreMdp" type="password" name="password" required></br>
             <button id="connexionBtn" type="submit" name="action" value="connexion">Connexion</buttom>
         </form>
         <form id="send_to_inscription" method="POST">
