f44e69c3f2231e4e9b962017f63a1423b2071a37
Thomas Nicolas <77977775+KaiDen26@users.noreply.github.com>
2022-06-01 00:00:28 +0200

    Update bddFinal.sql

--- Good2Watch/Base de données/bddFinal.sql
+++ Good2Watch/Base de données/bddFinal.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Hôte : localhost:3308
--- Généré le : ven. 27 mai 2022 à 17:20
--- Version du serveur :  5.7.33
--- Version de PHP : 7.4.14
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -16,10 +7,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `gonaya`
---
 
 -- --------------------------------------------------------
 
